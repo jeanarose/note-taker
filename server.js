b27fd687b3b23ee9a67d79bcae27d2970e33b2b9
Jeana Rose <jr.mathis608@gmail.com>
2021-01-18 14:20:47 -0500

    Added two routes that return index.html and notes.html.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const fs = require("fs");
 const database = require("./db/db.json");
+const path = require("path");
 
 // Define a PORT to listen on
 const PORT = 3000;
@@ -12,3 +13,22 @@ const app = express();
 // Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// ================================
+// *******   ROUTES   *******
+// ================================
+
+// Route that returns the index.html file
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Route that returns the notes.html file.
+app.get("/public/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log(`App listening on PORT " + ${PORT}.`);
+});
